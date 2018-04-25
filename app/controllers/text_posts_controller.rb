0ba654e20621a69a7ab4261dc2bb06b1a08c3628
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-04-26 01:26:48 +1000

    added edit to text posts

--- app/controllers/text_posts_controller.rb
+++ app/controllers/text_posts_controller.rb
@@ -3,6 +3,7 @@ class TextPostsController < ApplicationController
     @text_post = TextPost.new
   end
 
+
   def create
     @text_post = current_user.text_posts.build(text_post_params)
 
@@ -11,6 +12,22 @@ class TextPostsController < ApplicationController
 
     else
       render :new, alert: "Error creating post."
+    end
+
+  end
+
+  def edit
+    @text_post = current_user.text_posts.find(params[:id]))
+
+  end
+
+  def update
+    @text_post = current_user.text_posts.find(params[:id])
+    if @text_post.update(text_post_params)
+      redirect_to post_path(@text_post), notice: "Post updated!"
+
+    else
+      render :edit, alert:"Error updating post."
     end
 
   end
